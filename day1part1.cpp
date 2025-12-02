802586cfaf249f6fd0ce34f22feacc62e8cc792c
Manish <manishsahu6961@gmail.com>
2025-12-02 23:25:05 +0530

    completed day 2

---
+++ day1part1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "../algo/debug.h"
+#else
+#define debug(...) (void)0
+#endif
+#define all(v) v.begin(),v.end()
+#define int long long int
+#define IMAX INT64_MAX
+#define IMIN INT64_MIN
+#define endl '\n'
+const int MOD =(int)1000000000+7;
+using namespace std;
+void cases();
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(0);
+  clock_t z = clock();
+#ifndef ONLINE_JUDGE
+  freopen("input.txt" , "r", stdin );
+  freopen("output.txt", "w", stdout);
+  freopen("error.txt" , "w", stderr);
+#endif
+  cases();
+  cerr << "Run time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+}
+void cases()
+{
+  int position=50;
+  int answer=0;
+  string line;
+  while(getline(cin,line)){
+    stringstream ss;
+    ss<<line;
+    char direction;
+    ss>>direction;
+    int rotation;
+    ss>>rotation;
+    if(direction=='L'){
+      position+=100-(rotation%100);
+    }else{
+      position+=rotation;
+    }
+    position%=100;
+    if(position==0){
+      answer++;
+    }
+  }
+  cout<<answer<<endl;
+  return;
+}
---
+++ day2part1.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "../algo/debug.h"
+#else
+#define debug(...) (void)0
+#endif
+#define all(v) v.begin(), v.end()
+#define int long long int
+#define IMAX INT64_MAX
+#define IMIN INT64_MIN
+#define endl '\n'
+const int MOD = (int)1000000000 + 7;
+using namespace std;
+class solution
+{
+    vector<pair<int, int>> range;
+
+public:
+    solution();
+    void readInput();
+    void solve();
+};
+void solution::readInput()
+{
+    string s;
+    getline(cin, s);
+    stringstream ss;
+    ss << s;
+    char noise;
+    while (ss)
+    {
+        int start, end;
+        ss >> start;
+        ss >> noise;
+        ss >> end;
+        if (ss)
+            ss >> noise;
+        range.emplace_back(make_pair(start, end));
+    }
+}
+solution::solution()
+{
+    readInput();
+    solve();
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    clock_t z = clock();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+    solution sol;
+    cerr << "Run time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+}
+void solution::solve()
+{
+    set<int> s;
+    for (int i = 1; i < 1e6; i++)
+    {
+        s.insert(i * pow(10, 1 + (int)log10(i)) + i);
+    }
+    vector<int> st(s.begin(), s.end());
+    vector<int> psum(s.size() + 1, 0);
+    for (int i = 1; i < psum.size(); i++)
+    {
+        psum[i] = psum[i - 1] + st[i - 1];
+    }
+    int res = 0;
+    for (auto p : range)
+    {
+        int invalidSum = 0;
+        int f = lower_bound(st.begin(), st.end(), p.first) - st.begin();
+        int l = upper_bound(st.begin(), st.end(), p.second) - st.begin();
+        invalidSum=psum[l]-psum[f];
+        res+=invalidSum;
+    }
+    cout<<res<<endl;
+    return;
+}
